fcf1c9959c44357989eb0a01dc5a87cce832966d
Jessica Yu <yulejessica@gmail.com>
2022-05-18 16:10:35 -0700

    fixing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,29 @@ require("dotenv").config();
 const path = require('path');
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ENTRY_DIR = path.join(__dirname, "/client/src/index.jsx");
-const OUTPUT_DIR = path.join(__dirname, "/client/dist");
-const TEMPLATE_DIR = path.join(__dirname, "/client/src/index.html");
 
 module.exports = {
-  entry: ENTRY_DIR,
+  entry: path.join(__dirname, "/client/src/index.jsx"),
   output: {
-    path: OUTPUT_DIR,
+    path: path.resolve(__dirname, "/client/dist"),
     filename: "bundle.js"
   },
   module: {
     rules: [
       {
-        test: /\.(jsx|js)$/,
+        test: /\.(jsx|js)?/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            // plugins: [
+            //   ["@babel/plugin-transform-runtime",
+            //     {
+            //       "regenerator": true
+            //     }
+            //   ]
+            // ]
           }
         }
       },
@@ -28,7 +32,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: TEMPLATE_DIR,
+      template: path.join(__dirname, "/client/dist/index.html"),
     }),
   ],
 };
