4b18f29158814b0cff1237c10521f00f55ef44a0
Joshua Ham <Joshuaju.ham@gmail.com>
2022-05-18 16:17:48 -0700

    revert

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,26 @@ require("dotenv").config();
 const path = require('path');
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ENTRY_DIR = path.join(__dirname, "/client/src/index.jsx");
+const OUTPUT_DIR = path.join(__dirname, "/client/dist");
+const TEMPLATE_DIR = path.join(__dirname, "/client/src/index.html");
+//test
 
 module.exports = {
-  entry: path.join(__dirname, "/client/src/index.jsx"),
+  entry: ENTRY_DIR,
   output: {
-    path: path.resolve(__dirname, "/client/dist"),
+    path: OUTPUT_DIR,
     filename: "bundle.js"
   },
   module: {
     rules: [
       {
-        test: /\.(jsx|js)?/,
+        test: /\.(jsx|js)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            // plugins: [
-            //   ["@babel/plugin-transform-runtime",
-            //     {
-            //       "regenerator": true
-            //     }
-            //   ]
-            // ]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       },
@@ -32,7 +29,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "/client/dist/index.html"),
+      template: TEMPLATE_DIR,
     }),
   ],
-};
+}
