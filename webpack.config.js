1a36383cdb167e3dccbe1fa11ef2a41e06133e54
Jessica Yu <yulejessica@gmail.com>
2022-05-25 14:41:02 -0700

    deleted duplicated files

--- webpack.config.js
+++ webpack.config.js
@@ -32,11 +32,11 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"]
       },
-      {
-        presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}],
-        ['@babel/preset-react', {targets: {node: 'current'}}]]
-      }
+      // {
+      //   presets: [
+      //   ['@babel/preset-env', {targets: {node: 'current'}}],
+      //   ['@babel/preset-react', {targets: {node: 'current'}}]]
+      // }
     ]
   },
   plugins: [
